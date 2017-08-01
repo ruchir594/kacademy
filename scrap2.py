3cc0921d8f08d1b7f8c062e2416fd1447c1839f0
ruchir594 <ruchir594@gmail.com>
2017-08-01 10:18:29 +0530

    new scrap2 for less confusion 2

--- scrap2.py
+++ scrap2.py
@@ -21,7 +21,7 @@ khan = Khan(lang="en")
 with open('kaca_exer.csv', 'wb') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(['l0','l1','l2','n1','n2','u1','u2','video_title','youtube_id','mp4_id','mp4_low_id','png_id'])
+    spamwriter.writerow(['l0','l1','l2','n1','n2','u1','u2','video_title','png_id','exercise_id'])
     # creating the CSV file and wiring first line
     for e0 in allchar:
         print '~~~~~~~~~~~~~~'
@@ -37,31 +37,7 @@ with open('kaca_exer.csv', 'wb') as csvfile:
                 c = khan.get_topic_videos(e2['id'])
                 c_exercise = khan.get_topic_exercises(e2['id'])
                 # - going third layer in
-                """for each in c:
-                    youtube_id = "https://www.youtube.com/watch?v="+each["translated_youtube_id"]
-                    mp4_id = each["download_urls"]["mp4"]
-                    mp4_low_id = 'Does not exist'
-                    try:
-                        mp4_low_id = each["download_urls"]["mp4-low"]
-                    except Exception:
-                        mp4_low_id = 'Does not exist'
-                    png_id = each["download_urls"]["png"]
-                    vid_title = each["translated_title"]
-                    # writing into CSV file
-                    spamwriter.writerow([
-                        e0.replace(',','').encode('utf-8'),
-                        e1['id'].replace(',','').encode('utf-8'),
-                        e2['id'].replace(',','').encode('utf-8'),
-                        e1['title'].replace(',','').encode('utf-8'),
-                        e2['title'].replace(',','').encode('utf-8'),
-                        e1['url'].replace(',','').encode('utf-8'),
-                        e2['url'].replace(',','').encode('utf-8'),
-                        vid_title.replace(',','').encode('utf-8'),
-                        youtube_id,
-                        mp4_id,
-                        mp4_low_id,
-                        png_id
-                        ])"""
+
                 #print 'exercises....'
                 for each in c_exercise:
                     image_url = each["image_url"]
